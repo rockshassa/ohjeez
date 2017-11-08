ee89b0be9f75290f6dd01623ff741455c0a1bd30
Nick Galasso <nick@airtime.com>
2017-11-09 09:28:58 +1100

    Initial Commit

---
+++ ohjeez/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ohjeez
+//
+//  Created by Nick Galasso on 11/9/17.
+//  Copyright © 2017 Nick Galasso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

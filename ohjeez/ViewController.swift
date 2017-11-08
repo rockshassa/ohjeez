8e535bc90e153be97b77bef9e454fea98933f67f
Nick Galasso <nick@airtime.com>
2017-11-09 09:53:39 +1100

    the colors, duke

--- ohjeez/ViewController.swift
+++ ohjeez/ViewController.swift
@@ -6,20 +6,49 @@
 //  Copyright © 2017 Nick Galasso. All rights reserved.
 //
 
-import UIKit
+import GLKit
 
-class ViewController: UIViewController {
+class ViewController: GLKViewController {
 
+    var cc:[Float] = [1,1,0,1]
+    
+    var glkView:GLKView {
+        get {
+            return self.view as! GLKView
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let ctx = EAGLContext(api: .openGLES2) {
+            self.glkView.context = ctx
+        }
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(onTap(sender:)))
+        tap.numberOfTapsRequired = 1;
+        self.view.addGestureRecognizer(tap)
     }
-
+    
+    //selectors are an obj-c thing, so expose this function using @objc
+    @objc func onTap(sender:UITapGestureRecognizer){
+        if 0.0 == cc[0] {
+            cc[0] = 1.0
+            cc[2] = 0.0
+        } else {
+            cc[0] = 0.0
+            cc[2] = 1.0
+        }
+    }
+    
+    override func glkView(_ view: GLKView, drawIn rect: CGRect) {
+        glClearColor(cc[0], cc[1], cc[2], cc[3])
+        glClear(GLbitfield(GL_COLOR_BUFFER_BIT)) 
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
